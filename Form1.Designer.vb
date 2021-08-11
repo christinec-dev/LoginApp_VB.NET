12eac02b9abbe88a2264f6cf0f77f691947c9470
Christine Coomans <87696858+christinec-dev@users.noreply.github.com>
2021-08-11 16:42:55 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PasswordBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.UserNameBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LoginBtn = New System.Windows.Forms.Button()
+        Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.ClearBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'PasswordBox
+        '
+        Me.PasswordBox.Location = New System.Drawing.Point(193, 166)
+        Me.PasswordBox.Name = "PasswordBox"
+        Me.PasswordBox.Size = New System.Drawing.Size(327, 22)
+        Me.PasswordBox.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(118, 171)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Password:"
+        '
+        'UserNameBox
+        '
+        Me.UserNameBox.Location = New System.Drawing.Point(193, 121)
+        Me.UserNameBox.Name = "UserNameBox"
+        Me.UserNameBox.Size = New System.Drawing.Size(327, 22)
+        Me.UserNameBox.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(112, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "UserName:"
+        '
+        'LoginBtn
+        '
+        Me.LoginBtn.Location = New System.Drawing.Point(121, 230)
+        Me.LoginBtn.Name = "LoginBtn"
+        Me.LoginBtn.Size = New System.Drawing.Size(147, 31)
+        Me.LoginBtn.TabIndex = 5
+        Me.LoginBtn.Text = "Login"
+        Me.LoginBtn.UseVisualStyleBackColor = True
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.Location = New System.Drawing.Point(274, 230)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(129, 31)
+        Me.ExitBtn.TabIndex = 6
+        Me.ExitBtn.Text = "Exit"
+        Me.ExitBtn.UseVisualStyleBackColor = True
+        '
+        'ClearBtn
+        '
+        Me.ClearBtn.Location = New System.Drawing.Point(409, 230)
+        Me.ClearBtn.Name = "ClearBtn"
+        Me.ClearBtn.Size = New System.Drawing.Size(111, 31)
+        Me.ClearBtn.TabIndex = 7
+        Me.ClearBtn.Text = "Clear"
+        Me.ClearBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(622, 414)
+        Me.Controls.Add(Me.ClearBtn)
+        Me.Controls.Add(Me.ExitBtn)
+        Me.Controls.Add(Me.LoginBtn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.UserNameBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PasswordBox)
+        Me.Name = "Form1"
+        Me.Text = "3nm,"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PasswordBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents UserNameBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LoginBtn As Button
+    Friend WithEvents ExitBtn As Button
+    Friend WithEvents ClearBtn As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub LoginBtn_Click(sender As Object, e As EventArgs) Handles LoginBtn.Click
+        'Default username and password
+        Dim password As String = "pass123"
+        Dim user As String = "admin"
+        'Defines the first login number 
+        Dim i As Integer = 1
+
+        'The Do loop will loop until the username and password is correct
+        Do
+            If (UserNameBox.Text = user) AndAlso (PasswordBox.Text = password) Then
+                MsgBox("Logged In", MsgBoxStyle.OkCancel, "Valid")
+                Exit Do
+                'If incorrect, it will alert the user of this and give them two more boxes to enter the username and password
+            Else
+                MsgBox("Password or Username is incorrect. Try again", MsgBoxStyle.RetryCancel, "Invalid")
+                user = InputBox("Enter Username", "UserName")
+                password = InputBox("Enter Password", "Password")
+            End If
+
+            'We start at one attempt
+            i += 1
+            'If the attempt reaches 3 tries, the app will close
+        Loop Until i = 3
+        If (i = 3) Then
+            MsgBox("You've exceeded your number of tries. Bye.", MsgBoxStyle.OkOnly, "Invalid")
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub ExitBtn_Click(sender As Object, e As EventArgs) Handles ExitBtn.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CloseBtn_Click(sender As Object, e As EventArgs) Handles ClearBtn.Click
+        UserNameBox.Text = ""
+        PasswordBox.Text = ""
+    End Sub
+End Class
